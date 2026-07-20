5797561cfa221c0af3951556142beb501f380e58
Ilya <rosette0126@gmail.com>
2026-07-20 09:00:48 +0300

    add: DI

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,46 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:partfolio_app/core/theme/app_theme.dart';
-import 'package:partfolio_app/feautures/auth/presentation/login_screen.dart';
-import 'package:partfolio_app/feautures/tasks/presentation/tasks_screen.dart';
-import 'package:partfolio_app/feautures/tasks/presentation/bottom_navigation_bar.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'core/initialization/auth_gate.dart';
+import 'core/initialization/models/dependencies_container.dart';
+import 'core/initialization/widgets/dependencies_scope.dart';
+import 'core/service/auth_service.dart';
+import 'feautures/auth/data/data_source/local_auth_datasource_impl.dart';
+import 'feautures/auth/data/mapper/user_mapper.dart';
+import 'feautures/auth/data/repository/authentication_repository_impl.dart';
+import 'feautures/tasks/data/datasource/local_tasks_data_source_impl.dart';
+import 'feautures/tasks/data/mapper/tasks_mapper.dart';
+import 'feautures/tasks/data/repository/local_tasks_repository_impl.dart';
 
 void main() {
-  runApp(const MyApp());
-}
+  final authService = AuthService();
+  final sharedPreferences = SharedPreferencesAsync();
+  // Add: Dependencies container
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  WidgetsFlutterBinding.ensureInitialized();
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      themeMode: ThemeMode.light,
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.themeDataLight,
-      home: HomeScreen(),
-    );
-  }
+  runApp(
+    DependenciesScope(
+      dependencies: DependenciesContainer(
+        tasksRepository: TasksRepositoryImpl(
+          dataSource: LocalTasksDataSourceImpl(
+            sharedPreferences: sharedPreferences,
+          ),
+          tasksMapper: TasksMapper(),
+        ),
+        authService: authService,
+        sharedPreferencesAsync: sharedPreferences,
+        authenticationRepository: AuthenticationRepositoryImpl(
+          dataSource: LocalAuthDatasourceImpl(
+            authService: authService,
+            sharedPreferences: sharedPreferences,
+          ),
+          mapper: UserMapper(),
+        ),
+      ),
+      child: AuthGate(),
+    ),
+  );
 }
